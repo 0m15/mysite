f9ef00bc9c18c61d5ac95223452335df6de315a2
Simone Carella <simonecarella@gmail.com>
2018-08-05 15:54:24 +0200

    add analytics and offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,33 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-29310174-2",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "simonecarella.com",
+        short_name: "simc",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#222222",
+        display: "minimal-ui",
+        // icon: "src/images/icon.png", // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
   ],
