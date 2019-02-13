258e0cb08dd9b061a52988543bfa007ffa8d5436
Simone Carella <simonecarella@gmail.com>
2019-02-13 15:11:10 +0100

    add more lnks

--- src/pages/works.js
+++ src/pages/works.js
@@ -23,6 +23,10 @@ class IndexPage extends React.Component {
       <div className="w-100 z-999">
         <div className="pt6 mw8 ph4 center f4 fade">
           <h2 className="f1 fw3 lh-solid">Works &<br/> research</h2>
+          <p>
+              Selezione di lavori ed esperimenti, realizzati negli ultimi ~5 anni.<br/>
+              Link esterno a dropbox con ulteriori screenshot e video: <a className="black" href="https://www.dropbox.com/sh/yxnxxt9a2xb9shv/AACcyA8JTbojsN_Ul-8s4IIGa?dl=0" target="blank">Apri</a>
+          </p>
           <div className="pt4">
             <h3 className="fw2 f3 pb3">Web Apps/Sites</h3>
             <ul className="ma0 pa0">
@@ -32,13 +36,12 @@ class IndexPage extends React.Component {
                 subtitle="Progressive Web App per tablet iPad per evento fieristico a Parigi e fornita agli operatori commerciali di Bormioli Pharma. Sviluppata in breve tempo (meno di 2 settimane) con React/Next. Supporto offline, animazioni con TweenMax e fisica custom in Javascript per scroll e drag."
               />
               <WorkLine
-                video
-                url=""
+                url="https://www.dropbox.com/sh/mvkxjy751d9sx5p/AABFPSDURO6gype3sbAYoHqta?dl=0"
                 title="Intellitower – Celli Group"
                 subtitle="Interfaccia per sistema embedded realizzata per Celli @ Accurat. L'interfaccia è direttamente integrata nell'erogatore di bevande Intellitower, distribuito nel mercato asiatico da Celli Group. Il tutto gira su un sistema embedded basato su Yocto Linux e Chromium."
               />
               <WorkLine
-                url="http://hiddentribe.us"
+                url="http://hiddentribes.us"
                 title="The Hidden Tribes"
                 subtitle="Sito per il report di Hidden Tribes, sviluppato @ Accurat."
               />
